19ce428ae2199b94c3c556dc3a93f3baafa43ea8
Ricardo Alvize <Ricardo Alvize@DESKTOP-1PARVDT>
2017-12-05 22:20:05 -0200

    Falta Terminar Autenticação e Autorização

--- ProjetoLavacaoStreetCar/Migrations/201712060000200_CreateUser.cs
+++ ProjetoLavacaoStreetCar/Migrations/201712060000200_CreateUser.cs
@@ -9,13 +9,13 @@ namespace ProjetoLavacaoStreetCar.Migrations
         {
             Sql(@"INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'3f8552ec-b465-472d-84ed-a95d5a422297', N'ricardo.alvize@gmail.com', 0, N'AHW+4qCzO+z68vxZqhWZPistsMH4iSmzt7JrMwYGRcWvOeDaKG/1py408aPxqonDPw==', N'c270c525-226d-4746-9b4a-3be9995b2e22', NULL, 0, 0, NULL, 1, 0, N'ricardo.alvize@gmail.com')
                 
-                
+               INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'28515ddc-fc54-4a47-9c10-9b987dd2ec12', N'CanManageCustomers')
 
-                  INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'28515ddc-fc54-4a47-9c10-9b987dd2ec12', N'CanManageCustomers')
-
-
+               INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'3f8552ec-b465-472d-84ed-a95d5a422297', N'28515ddc-fc54-4a47-9c10-9b987dd2ec12')
 
        ");
+
+ 
 
         }
         
